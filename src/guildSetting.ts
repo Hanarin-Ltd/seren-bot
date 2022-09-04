a6d1860ac10e4b6de17e39ea7d94e322d9517823
cottons-kr <cottons-kr@naver.com>
2022-09-04 15:14:09 +0900

    대시보드 주소 수정

--- src/guildSetting.ts
+++ src/guildSetting.ts
@@ -9,11 +9,11 @@ import { setDefaultGuildOption } from "./utils/guildOption"
 import { setDefaultGuildPermission } from "./utils/permission"
 import { addAllGuildRole } from "./utils/role"
 
-const thanksForUsing = () => {
+const thanksForUsing = (guildId: string) => {
     return new EmbedBuilder()
         .setColor(BOT_COLOR)
         .setTitle(':wave: Seren을 선택해주셔서 감사합니다!')
-        .setURL(env.SITE + '/login')
+        .setURL(`${env.SITE}/dashboard/${guildId}`)
         .setDescription('**사용해주셔서 감사합니다!**\nSeren을 사용하기전 기본적인 설정이 필요합니다. 링크로 들어가 로그인 한뒤 설정을 해주세요.\n이 설정은 웹 대시보드에서 변경할 수 있습니다.')
 }
 
@@ -26,5 +26,5 @@ export default async function guildSetting(guild: Guild) {
     await addAllGuildChannel(guild)
     await addAllGuildRole(guild)
 
-    await (await getGuildOwner(guild)).send({ embeds: [thanksForUsing()] })
+    await (await getGuildOwner(guild)).send({ embeds: [thanksForUsing(guildId)] })
 }
