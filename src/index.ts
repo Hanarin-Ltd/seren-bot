3e41b8bb13ac25cfdc3e593ca19e6e5436d624ce
cottons-kr <cottons-kr@naver.com>
2022-10-11 21:46:52 +0900

    한디리 지원

--- src/index.ts
+++ src/index.ts
@@ -41,24 +41,22 @@ const clientIntents = [
     GatewayIntentBits.GuildMessageTyping,
     GatewayIntentBits.DirectMessageTyping
 ]
-const KOREAN_BOT_TOKEN = env.KOREAN_TOKEN
-let client: Client | null = null
+const KOREAN_TOKEN = env.KOREAN_TOKEN
+export let client: Client = new Client({ intents: clientIntents })
 
 if(env.NODE_ENV === 'production'){
     client = new KoreanbotsClient({ 
         intents: clientIntents,
         koreanbots: {
             api: {
-                token: KOREAN_BOT_TOKEN
+                token: KOREAN_TOKEN!
             }
         },
         koreanbotsClient: {
             updateInterval: 600000 //10분마다 서버 수를 업데이트합니다. (기본값 30분)
         }
     })
-  } else {
-    client = new Client({ intents: clientIntents })
-  }
+}
 
 
 client.on('ready', async () => {
